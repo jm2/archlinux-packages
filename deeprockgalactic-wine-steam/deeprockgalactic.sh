f794562981486770d5148e67082c6330ddf02c59
John-Michael Mulesa <jmulesa@gmail.com>
2021-09-17 12:16:51 +1000

    Remove DRG.

--- deeprockgalactic-wine-steam/deeprockgalactic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export WINEPREFIX="$HOME"/.deeprockgalactic/wine
-if [ ! -d "$HOME"/.deeprockgalactic ] ; then
-   mkdir -p "$HOME"/.deeprockgalactic/wine
-   wineboot -u
-   winetricks dxvk
-fi
-cd /opt/deeprockgalactic
-/usr/bin/wine FSD.exe
