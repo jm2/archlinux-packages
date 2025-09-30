acc128f9fb5f8ea9a861d036a2f3716781616b3f
John-Michael Mulesa <jmulesa@gmail.com>
2025-09-30 19:01:19 -0400

    Update wreckfest2 to use umu-launcher.

--- wreckfest2-wine-steam/wreckfest2.sh
+++ wreckfest2-wine-steam/wreckfest2.sh
@@ -2,8 +2,6 @@
 export WINEPREFIX="$HOME"/.wreckfest2/wine
 if [ ! -d "$HOME"/.wreckfest2 ] ; then
    mkdir -p "$HOME"/.wreckfest2/wine
-   wineboot -u
-   setup_dxvk install
 fi
 cd /opt/wreckfest2
-/usr/bin/wine Wreckfest2.exe
+/usr/bin/umu-run Wreckfest2.exe
